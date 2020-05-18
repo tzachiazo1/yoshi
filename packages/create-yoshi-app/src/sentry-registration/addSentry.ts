be050203e1212ce4362c73bd7def40c79f5d6db7
Artem Yavorsky <info@yavorsky.org>
2020-05-18 12:51:57 +0300

    Create temporary pkg.json for sentry configuration (#2425)
    * Create temporary pkg.json for sentry configuration
    
    * Update addSentry.ts

--- packages/create-yoshi-app/src/sentry-registration/addSentry.ts
+++ packages/create-yoshi-app/src/sentry-registration/addSentry.ts
@@ -1,4 +1,21 @@
+import path from 'path';
+import fs from 'fs-extra';
 import Listr from 'listr';
+
+// We need to mock pkg.json in current directory, so add-sentry tool will find this file and use for configuration
+const PKG_FILE_NAME = path.join(process.cwd(), 'package.json');
+const addPackageJSONMock = () => {
+  const tempPkgContent = JSON.stringify({
+    devDependencies: {
+      typescript: 'latest',
+    },
+  });
+  fs.writeFileSync(PKG_FILE_NAME, tempPkgContent);
+};
+
+const removePackageJSONMock = () => {
+  fs.unlinkSync(PKG_FILE_NAME);
+};
 
 export const addSentry = ({
   teamName,
@@ -7,12 +24,14 @@ export const addSentry = ({
   teamName: string;
   projectName: string;
 }) => {
+  addPackageJSONMock();
   const tasks = require('@wix/add-sentry/lib/tasks');
   const {
     configureSentry,
     handleError,
   } = require('@wix/add-sentry/lib/sentry');
 
+  removePackageJSONMock();
   configureSentry();
 
   const tasksToExecute = [
