1f6c97b6e7fa2226243ec122fcdb5aec96cc5374
Artem <bordyanartem@gmail.com>
2020-05-25 19:08:36 +0300

    Update js-sdk; Update iframe-bi-logger (#2470)
    * chore(flow-editor): Update iframe-logger; Update js-sdk
    
    * chore(flow-editor): Update js-sdk; update iframe-bi-logger
    
    * chore(create-yoshi-app): Rollback package.json

--- packages/yoshi-flow-editor-runtime/src/constants.ts
+++ packages/yoshi-flow-editor-runtime/src/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_WIX_SDK_SRC =
-  'https://static.parastorage.com/services/js-sdk/1.190.0/js/wix-private.min.js';
+  'https://static.parastorage.com/services/js-sdk/1.469.0/js/wix-private.min.js';
 
 export const OOI_WIDGET_COMPONENT_TYPE = 'WIDGET_OUT_OF_IFRAME';
 export const PLATFORM_WIDGET_COMPONENT_TYPE = 'STUDIO_WIDGET';
